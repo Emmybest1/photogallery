20634f4796e00e98a52087d884c14564f928ba40
Emmanuel Onah <emmajerry2016@gmail.com>
2020-11-10 12:40:35 +0200

    fixed module issue

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import HomePage from './pages/home.page';
-import Testing from './pages/testing.component'
 
 const App: React.FC = (): JSX.Element => {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route exact path="/test" component={Testing} />
       </Switch>
     </Router>
   );
